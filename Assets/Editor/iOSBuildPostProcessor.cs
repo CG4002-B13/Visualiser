71846643b68efa12716bfafcae429f43a23db1aa
Parth Gandhi <parthgandhi22@u.nus.edu>
2025-11-01 23:47:47 +0800

    Edit file to enable permission to save app images in IOS

--- Assets/Editor/iOSBuildPostProcessor.cs
+++ Assets/Editor/iOSBuildPostProcessor.cs
@@ -5,6 +5,7 @@ using UnityEditor.Callbacks;
 using UnityEditor.iOS.Xcode;
 using System.IO;
 
+
 public class iOSBuildPostProcessor
 {
     [PostProcessBuild(100)]
@@ -13,34 +14,81 @@ public class iOSBuildPostProcessor
         if (buildTarget != BuildTarget.iOS)
             return;
 
+
         Debug.Log("iOS Post Process: Starting...");
+
 
         // Get the project path
         string projectPath = pathToBuiltProject + "/Unity-iPhone.xcodeproj/project.pbxproj";
+
 
         // Load the Xcode project
         PBXProject project = new PBXProject();
         project.ReadFromFile(projectPath);
 
+
         // Get BOTH target GUIDs
         string mainTargetGuid = project.GetUnityMainTargetGuid();
         string frameworkTargetGuid = project.GetUnityFrameworkTargetGuid();
+
 
         // Add Photos framework to MAIN target
         project.AddFrameworkToProject(mainTargetGuid, "Photos.framework", false);
         project.AddFrameworkToProject(mainTargetGuid, "PhotosUI.framework", false);
 
+
         Debug.Log("iOS Post Process: Added Photos frameworks to Unity-iPhone target");
+
 
         // Add Photos framework to FRAMEWORK target (this is the important one!)
         project.AddFrameworkToProject(frameworkTargetGuid, "Photos.framework", false);
         project.AddFrameworkToProject(frameworkTargetGuid, "PhotosUI.framework", false);
 
+
         Debug.Log("iOS Post Process: Added Photos frameworks to UnityFramework target");
+
 
         // Write the modified project back
         project.WriteToFile(projectPath);
 
+
+        // ===== NEW: Add Info.plist permissions =====
+        // Get the Info.plist file path
+        string infoPlistPath = pathToBuiltProject + "/Info.plist";
+
+
+        // Load the plist file
+        PlistDocument plist = new PlistDocument();
+        plist.ReadFromFile(infoPlistPath);
+
+
+        // Get root dict
+        PlistElementDict rootDict = plist.root;
+
+
+        // Add Photo Library Add Usage Description (for saving to Photos)
+        if (!rootDict.values.ContainsKey("NSPhotoLibraryAddUsageDescription"))
+        {
+            rootDict.SetString("NSPhotoLibraryAddUsageDescription",
+                "This app needs access to your photos to save screenshots to your Photos library.");
+            Debug.Log("iOS Post Process: Added NSPhotoLibraryAddUsageDescription");
+        }
+
+
+        // Add Photo Library Usage Description (for reading from Photos - optional but good practice)
+        if (!rootDict.values.ContainsKey("NSPhotoLibraryUsageDescription"))
+        {
+            rootDict.SetString("NSPhotoLibraryUsageDescription",
+                "This app needs access to your photos to view and manage your screenshots.");
+            Debug.Log("iOS Post Process: Added NSPhotoLibraryUsageDescription");
+        }
+
+
+        // Save the modified plist
+        plist.WriteToFile(infoPlistPath);
+
+
+        Debug.Log("iOS Post Process: Info.plist permissions added successfully!");
         Debug.Log("iOS Post Process: Completed successfully!");
     }
 }
